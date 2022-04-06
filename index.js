e315c9849d6b9a58065750d7075bd0ff746a5818
Julian Richardson <jrichar3@yahoo.com>
2022-04-06 15:11:58 -0400

    added all the questions for readme

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const genMarkdown = ({name}) =>
+const genMarkdown = ({name, description, install, instructions, contributors,licenses, features, test}) =>
 `# <${name}>
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+${description}
 
 ## Table of Contents (Optional)
 
@@ -23,51 +18,34 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${install}
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    
+${instructions}
+   
     ![alt text](assets/images/screenshot.png)
     
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+${contributors}
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${licenses}
 
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
 ## Features
 
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${features}
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+${test}`
 
 inquirer
     .prompt([
@@ -76,12 +54,42 @@ inquirer
             name: 'name',
             message: 'What is the title of your project?',
         },
-        // {
-        //     type: 'checkbox',
-        //     message: 'What languages do you know?',
-        //     name: 'stack',
-        //     choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-        // },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a short description explaining your project...',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Please provide instructions for use of your project...',
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Are there any project contributors that you would like to include in your README?',
+        },
+        {
+            type: 'checkbox',
+            message: 'Would you like to add any of the following licenses?',
+            name: 'licenses',
+            choices: [' MIT', ' Babel', ' Rails', ' Bash'],
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'Which features would you like to add?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'please provide examples of how to run project test...',
+        },
         // {
         //     type: 'list',
         //     message: 'What is your preferred method of communication?',
